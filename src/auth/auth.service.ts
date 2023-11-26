cc2653ad6e2e32dde1e0358d8960b54059ffbfdf
Hugo Orlando Gonzalez <hugo-orlando.gonzalez@atos.net>
2023-11-26 20:17:44 -0300

    :sparkles: added user auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, Body } from '@nestjs/common';
+import { Injectable, Body, ForbiddenException } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import * as argon from 'argon2';
 
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -14,17 +15,27 @@ export class AuthService {
     // generate password hash
     const hash = await argon.hash(password);
 
-    // save the new user in the db
-    const user = await this.prismaService.user.create({
-      data: {
-        email,
-        hash,
-      },
-    });
-    // provisory
-    delete user.hash;
-    // return the saved user
-    return user;
+    try {
+      // save the new user in the db
+      const user = await this.prismaService.user.create({
+        data: {
+          email,
+          hash,
+        },
+      });
+      // provisory
+      delete user.hash;
+
+      // return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
 
   signin() {
